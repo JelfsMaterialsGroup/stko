e047953cc0d0be3dc3204daf252b48c8662691b3
stevenbennett96 <stevenbennettonline@gmail.com>
2020-05-11 10:21:53 +0100

    Updated setup.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="stko-stevenb96",
+    name="stko",
     version="0.0.1",
     author="Steven Bennett, Andrew Tarzia",
     author_email="s.bennett18@imperial.ac.uk",
