968cc30aa18cd14dbc261ea4c217175ab9041240
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-08-14 14:35:26 +0100

    Implement working algorithm, needs improvement/testing.

--- stko/optimizers/collapser.py
+++ stko/optimizers/collapser.py
@@ -11,6 +11,9 @@ Optimizer for collapsing enlarged topologies.
 import logging
 from itertools import combinations
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import pdist
+import random
 import uuid
 import os
 import shutil
@@ -314,6 +317,12 @@ class Collapserv2(Collapser):
         output_dir,
         step_size,
         target_bond_length,
+        num_steps,
+        bond_epsilon=50,
+        nonbond_epsilon=20,
+        nonbond_sigma=1.2,
+        nonbond_mu=3,
+        beta=2,
     ):
         """
         Initialize a :class:`Collapser` instance.
@@ -332,9 +341,18 @@ class Collapserv2(Collapser):
             Target equilibrium bond length for long bonds to minimize
             to.
 
+        num_steps : :class:`int`
+            Number of MC moves to perform.
         self._output_dir = output_dir
         self._step_size = step_size
         self._target_bond_length = target_bond_length
+        self._num_steps = num_steps
+        self._bond_epsilon = bond_epsilon
+        self._nonbond_epsilon = nonbond_epsilon
+        self._nonbond_sigma = nonbond_sigma
+        self._nonbond_mu = nonbond_mu
+        self._beta = beta
+
     def _get_bb_atom_ids(self, mol):
 
         bb_ids = list(set([
@@ -394,6 +412,172 @@ class Collapserv2(Collapser):
 
         return bb_centroids
 
+    def _get_cent_to_lb_vector(
+        self,
+        mol,
+        bb_centroids,
+        long_bond_infos
+    ):
+        """
+        Returns dict of long bond atom to bb centroid vectors.
+
+        """
+
+        position_matrix = mol.get_position_matrix()
+        centroid_to_lb_vectors = {}
+        for bb in bb_centroids:
+            cent = bb_centroids[bb]
+            for b_atom_ids, bond_info in long_bond_infos.items():
+                for atom_id in b_atom_ids:
+                    atom_info, = mol.get_atom_infos(atom_ids=atom_id)
+                    atom_pos = position_matrix[atom_id]
+                    if atom_info.get_building_block_id() == bb:
+                        centroid_to_lb_vectors[(bb, atom_id)] = (
+                            atom_pos - cent,
+                        )
+                        break
+
+        return centroid_to_lb_vectors
+
+    def _bond_potential(self, distance):
+        """
+        Define an arbitrary parabolic bond potential.
+
+        This potential has no relation to an empircal forcefield.
+
+        """
+
+        potential = (distance - self._target_bond_length) ** 2
+        potential = self._bond_epsilon * potential
+        # force = -2 * (distance - self._target_bond_length)
+        # force = self._bond_epsilon * force
+
+        return potential
+
+    def _non_bond_potential(self, distance):
+        """
+        Define an arbitrary repulsive nonbonded potential.
+
+        This potential has no relation to an empircal forcefield.
+
+        """
+
+        return (
+            self._nonbond_epsilon * (
+                (self._nonbond_sigma/distance) ** self._nonbond_mu
+            )
+        )
+
+    def _compute_non_bonded_potential(self, mol):
+
+        # Get all pairwise distances.
+        pair_dists = pdist(mol.get_position_matrix())
+        non_bonded_potential = self._non_bond_potential(pair_dists)
+
+        return sum(non_bonded_potential)
+
+    def _compute_potential(self, mol, long_bond_infos):
+
+        system_potential = self._compute_non_bonded_potential(mol)
+        for long_bond_ids in long_bond_infos:
+            long_bond = long_bond_infos[long_bond_ids]
+
+            system_potential += self._bond_potential(
+                distance=self._get_bond_length(
+                    mol=mol,
+                    bond=long_bond.get_bond()
+                )
+            )
+
+        return system_potential
+
+    def _translate_atoms_along_vector(self, mol, atom_ids, vector):
+
+        new_position_matrix = mol.get_position_matrix()
+        for atom in mol.get_atom_infos(atom_ids=atom_ids):
+            id = atom.get_atom().get_id()
+            pos = mol.get_position_matrix()[id]
+            new_position_matrix[id] = pos - vector
+
+        return mol.with_position_matrix(new_position_matrix)
+
+    def _rotate_atoms_onto_vector(
+        self,
+        mol,
+        atom_ids,
+        start_vector,
+        target_vector,
+        axis
+    ):
+
+        return None
+
+    def _test_move(self, curr_pot, new_pot):
+
+        if new_pot < curr_pot:
+            return True
+        else:
+            exp_term = np.exp(-self._beta*(new_pot-curr_pot))
+            rand_number = random.random()
+
+            if exp_term > rand_number:
+                return True
+            else:
+                return False
+
+    def _output_top_lines(self):
+
+        string = (
+            '====================================================\n'
+            '                Collapser optimisation              \n'
+            '                ----------------------              \n'
+            '                                                    \n'
+            f' step size = {self._step_size} \n'
+            f' target bond length = {self._target_bond_length} \n'
+            f' num. steps = {self._num_steps} \n'
+            f' bond epsilon = {self._bond_epsilon} \n'
+            f' nonbond epsilon = {self._nonbond_epsilon} \n'
+            f' nonbond sigma = {self._nonbond_sigma} \n'
+            f' nonbond mu = {self._nonbond_mu} \n'
+            f' beta = {self._beta} \n'
+            '====================================================\n\n'
+        )
+
+        return string
+
+    def _plot_progess(self, steps, maxds, spots, npots, output_dir):
+
+        fig, ax = plt.subplots(figsize=(8, 5))
+        ax.plot(steps, maxds, c='k', lw=2)
+        # Set number of ticks for x-axis
+        ax.tick_params(axis='both', which='major', labelsize=16)
+        ax.set_xlabel('step', fontsize=16)
+        ax.set_ylabel('max long bond length [angstrom]', fontsize=16)
+        ax.axhline(y=self._target_bond_length, c='r')
+        fig.tight_layout()
+        fig.savefig(
+            os.path.join(output_dir, f'maxd_vs_step.pdf'),
+            dpi=360,
+            bbox_inches='tight'
+        )
+        plt.close()
+        # Plot energy vs timestep.
+        fig, ax = plt.subplots(figsize=(8, 5))
+        ax.plot(steps, spots, c='k', lw=2, label='total')
+        ax.plot(steps, npots, c='r', lw=2, label='non bonded')
+        # Set number of ticks for x-axis
+        ax.tick_params(axis='both', which='major', labelsize=16)
+        ax.set_xlabel('step', fontsize=16)
+        ax.set_ylabel('potential', fontsize=16)
+        ax.legend(fontsize=16)
+        fig.tight_layout()
+        fig.savefig(
+            os.path.join(output_dir, f'pot_vs_step.pdf'),
+            dpi=360,
+            bbox_inches='tight'
+        )
+        plt.close()
+
     def optimize(self, mol):
         """
         Optimize `mol`.
@@ -409,6 +593,8 @@ class Collapserv2(Collapser):
             The optimized molecule.
 
         """
+
+        random.seed()
 
         # Handle output dir.
         if self._output_dir is None:
@@ -428,67 +614,236 @@ class Collapserv2(Collapser):
         bb_atom_ids = self._get_bb_atom_ids(mol)
         bb_centroids = self._get_bb_centroids(mol, bb_atom_ids)
 
-                i.get_atom().get_id()
-            )
+        # Define bb centroid - long bond atom vectors.
+        # These are to be maintained during optimisation.
+        # centroid_to_lb_vectors = self._get_cent_to_lb_vector(
+        #     mol, bb_centroids, long_bond_infos
+        # )
+        print('###################################################')
+        print('WARNING: centroid_to_lb_vectors not maintained yet.')
+        print('###################################################')
 
-        # Translate each BB along BB_COM_vectors `step`.
-        # `step` is the proportion of the BB_COM_vectors that is moved.
-        step_no = 0
-        step = self._step_size
-        while not self._has_short_contacts(mol):
-            # Update each step the building block vectors and distance.
-            BB_cent_vectors, BB_cent_scales = self._get_BB_vectors(
-                mol=mol,
-                BB_atom_ids=BB_atom_ids
-            )
-
-            new_pos = self._get_new_position_matrix(
-                mol=mol,
-                step=step,
-                vectors=BB_cent_vectors,
-                scales=BB_cent_scales
-            )
-            step_no += 1
-            mol = mol.with_position_matrix(new_pos)
-            mol.write(
-                os.path.join(output_dir, f'collapsed_{step_no}.mol')
-            )
-
-        BB_cent_vectors, BB_cent_scales = self._get_BB_vectors(
+        system_potential = self._compute_potential(
             mol=mol,
-            BB_atom_ids=BB_atom_ids
+            long_bond_infos=long_bond_infos
         )
 
-        # Check that we have not gone too far.
-        min_dist = min(
-            dist for dist in self._get_inter_BB_distance(mol)
-        )
-        if min_dist < self._distance_cut / 2:
-            # Revert to half the previous step if we have.
-            step = -(self._step_size/2)
-            new_pos = self._get_new_position_matrix(
-                mol=mol,
-                step=step,
-                vectors=BB_cent_vectors,
-                scales=BB_cent_scales
-            )
-            step_no += 1
-            mol = mol.with_position_matrix(new_pos)
+        with open(os.path.join(output_dir, f'coll.out'), 'w') as f:
+            f.write(self._output_top_lines())
             mol.write(
-                os.path.join(output_dir, f'collapsed_rev.mol')
+                os.path.join(output_dir, f'testing_0.mol')
+            )
+            steps = [0]
+            passed = []
+            spots = [system_potential]
+            npots = [self._compute_non_bonded_potential(mol=mol)]
+            maxds = [max([
+                self._get_bond_length(
+                    mol, long_bond_infos[i].get_bond()
+                )
+                for i in long_bond_infos
+            ])]
+            f.write(
+                'Step system_potential nonbond_potential max_dist '
+                'opt_bbs updated?\n'
+            )
+            f.write(
+                f'{steps[-1]} {spots[-1]} {npots[-1]} {maxds[-1]} '
+                '-- --\n'
+            )
+            for step in range(1, self._num_steps):
+
+                # Randomly select a long bond.
+                lb_ids = random.choice(list(long_bond_infos.keys()))
+                lb_info = long_bond_infos[lb_ids]
+
+                lb_vector = self._get_bond_vector(
+                    mol=mol,
+                    bond=lb_info.get_bond()
+                )
+
+                bb_id_1, bb_id_2 = (
+                    i.get_building_block_id()
+                    for i in mol.get_atom_infos(atom_ids=lb_ids)
+                )
+
+                # Choose bb to move out of the two randomly.
+                moving_bb = random.choice([bb_id_1, bb_id_2])
+                moving_bb_atom_ids = tuple(
+                    i.get_atom().get_id()
+                    for i in mol.get_atom_infos()
+                    if i.get_building_block_id() == moving_bb
+                )
+
+                # Randomly choose between translation along long bond
+                # vector or along BB-COM vector.
+                # Random number from -1 to 1
+                rand = (random.random() - 0.5) * 2
+
+                # Define translation along long bond vector where
+                # direction is from force, magnitude is randomly
+                # scaled.
+                long_bond_translation = (
+                    -lb_vector * self._step_size * rand
+                )
+
+                # Get bb COM vector to molecule COM.
+                cent = mol.get_centroid()
+                bb_cent_vector = (
+                    mol.get_centroid(atom_ids=moving_bb_atom_ids)-cent
+                )
+                com_translation = (
+                    bb_cent_vector * self._step_size * rand
+                )
+
+                translation_vector = random.choice([
+                    long_bond_translation,
+                    com_translation,
+                ])
+
+                # Translate building block.
+                # Update atom position of building block.
+                mol = self._translate_atoms_along_vector(
+                    mol=mol,
+                    atom_ids=moving_bb_atom_ids,
+                    vector=translation_vector,
+                )
+
+                ###################################################
+                # Here I want to add rotations
+                # To maintain cent_vectors relative orientations.
+                # cent_vector_1 = centroid_to_lb_vectors[
+                #     (bb_id_1, lb_ids[0])
+                # ]
+                # cent_vector_2 = centroid_to_lb_vectors[
+                #     (bb_id_2, lb_ids[1])
+                # ]
+                ###################################################
+
+                new_system_potential = self._compute_potential(
+                    mol=mol,
+                    long_bond_infos=long_bond_infos
+                )
+
+                if self._test_move(
+                    system_potential,
+                    new_system_potential
+                ):
+                    updated = 'T'
+                    system_potential = new_system_potential
+                    passed.append(step)
+                else:
+                    updated = 'F'
+                    # Reverse move.
+                    mol = self._translate_atoms_along_vector(
+                        mol=mol,
+                        atom_ids=moving_bb_atom_ids,
+                        vector=-translation_vector,
+                    )
+
+                mol.write(
+                    os.path.join(output_dir, f'testing_{step}.xyz')
+                )
+                steps.append(step)
+                spots.append(system_potential)
+                npots.append(
+                    self._compute_non_bonded_potential(mol=mol)
+                )
+                maxds.append(max([
+                    self._get_bond_length(
+                        mol, long_bond_infos[i].get_bond()
+                    )
+                    for i in long_bond_infos
+                ]))
+                f.write(
+                    f'{steps[-1]} {spots[-1]} '
+                    f'{npots[-1]} {maxds[-1]} {lb_ids} {updated}\n'
+                )
+                step += 1
+
+            f.write('\n============================================\n')
+            f.write(
+                'Optimisation done:\n'
+                f'{len(passed)} steps passed: '
+                f'{len(passed)/self._num_steps}'
             )
 
-        out_file = os.path.join(output_dir, f'collapser.out')
-        with open(out_file, 'w') as f:
-            f.write(
-                f"Collapser algorithm.\n"
-                f"====================\n"
-                f"Step size: {self._step_size}\n"
-                f"Scale steps?: {self._scale_steps}\n"
-                f"Distance cut: {self._distance_cut}\n"
-                f"====================\n"
-                f"Steps run: {step_no}\n"
-                f"Minimum inter-BB distance: {min_dist}\n"
-                f"====================\n"
-            )
+        self._plot_progess(steps, maxds, spots, npots, output_dir)
+
         return mol
+
+#
+#
+#
+# print(lb_ids, lb_vector, lb_length)
+# bb_id_1, bb_id_2 = (
+#     i.get_building_block_id()
+#     for i in mol.get_atom_infos(atom_ids=lb_ids)
+# )
+# print(bb_id_1, bb_id_2)
+#
+# cent_vector_1 = centroid_to_lb_vectors[
+#     (bb_id_1, lb_ids[0])
+# ]
+# cent_vector_2 = centroid_to_lb_vectors[
+#     (bb_id_2, lb_ids[1])
+# ]
+# print(cent_vector_1, cent_vector_2)
+# if bb_id_1 in constrained_bbs and bb_id_2 in constrained_bbs:
+#     raise ValueError('issues!')
+# moving_bb = (
+#     bb_id_1 if bb_id_1 not in constrained_bbs else bb_id_2
+# )
+# constrained_bbs.add(moving_bb)
+# print('cbps', constrained_bbs)
+# # if len(constrained_bb_sets) == 0:
+# #     constrained_bb_sets[len(constrained_bb_sets)] = set(
+# #         [bb_id_1]
+# #     )
+# # else:
+# #     in_cbs = False
+# #     for cbs_id in constrained_bb_sets:
+# #         cbs = constrained_bb_sets[cbs_id]
+# #         print(cbs_id, constrained_bb_sets[cbs_id])
+# #         paired_bbs = []
+# #         if bb_id_1 in cbs:
+# #             paired_bbs = [i for i in cbs if i != bb_id_1]
+# #             for pbb in paired_bbs:
+# #                 moving_bbs.add(paired_bbs)
+# #             cbs.add(bb_id_1)
+# #             in_cbs = True
+# #         print(paired_bbs)
+# #     if not in_cbs:
+# #         constrained_bb_sets[len(constrained_bb_sets)] = set(
+# #             [bb_id_1]
+# #         )
+# #
+# # print('mbs', moving_bbs)
+# # print('cbps', constrained_bb_sets)
+# # input()
+# # Get all atoms in  building blocks to moved,
+# moving_bb_atom_ids = (
+#     i.get_atom().get_id()
+#     for i in mol.get_atom_infos()
+#     if i.get_building_block_id() == moving_bb
+# )
+#
+# # Move along lb_vector enought o decrease to target length
+# delta_dist = lb_length - self._target_bond_length
+# print(f's{step}', delta_dist)
+# translation_vector = -lb_vector * (delta_dist/(2*lb_length))
+#
+# # Update atom position of building block.
+# mol = self._translate_atoms_along_vector(
+#     mol=mol,
+#     atom_ids=moving_bb_atom_ids,
+#     vector=translation_vector,
+# )
+
+#
+# for long_bond_ids in long_bond_infos:
+#     long_bond = long_bond_infos[long_bond_ids]
+#     print(long_bond_ids, self._get_bond_length(
+#         mol=mol,
+#         bond=long_bond.get_bond()
+#     ))
