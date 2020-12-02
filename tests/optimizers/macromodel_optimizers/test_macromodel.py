c6078355038d9df9f5880aa62d03bb793ec15be8
Steven Bennett <s.bennett18@imperial.ac.uk>
2020-12-02 16:20:38 +0000

    Added first MacroModel test.

---
+++ tests/optimizers/macromodel_optimizers/test_macromodel.py
@@ -0,0 +1,41 @@
+from tests.fixtures.benzene import benzene_build
+import pytest 
+import sys
+from stko.optimizers.macromodel import MacroModel, MacroModelMD, MAEExtractor, MacroModelForceField
+
+
+macromodel_check = pytest.mark.skipif(
+    all('macromodel_path' not in x for x in sys.argv),
+    reason="Only run when MacroModel path is explicitly given."
+)
+
+@pytest.fixture(scope="function")
+def macromodel_md(macromodel_path):
+    return MacroModelMD(macromodel_path)
+
+@pytest.fixture(scope="function")
+def macromodel_forcefield(tmpdir, macromodel_path):
+    return MacroModelForceField(force_field=16, output_dir=tmpdir, macromodel_path=macromodel_path)
+
+    
+
+@macromodel_check
+class TestMacroModelFF:
+    def test_generate_com(self, macromodel_forcefield, tmp_path, mol=benzene_build):
+        # Write the ``.com`` file. 
+        run_name = 'test_com' 
+        macromodel_forcefield._generate_com(mol, str(tmp_path/run_name))
+        with open(str(tmp_path / (run_name+'.com')), 'r') as f:
+            com = f.readlines()
+        com_path = '/home/sbennett/PhD/stko/tests/optimizers/macromodel_optimizers/benzene_test.com'
+        with open(com_path, 'r') as f:
+            test_com = f.readlines()[2:]
+        # Do not compare first two lines, as these correspond 
+        assert test_com == com[2:]
+
+
+
+
+        
+
+
