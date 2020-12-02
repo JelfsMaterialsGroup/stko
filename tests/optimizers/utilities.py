1188647148ded2d03d6ec86e04a261701f18d431
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-12-02 14:05:40 +0000

    Implement compare molecules and fake test.

--- tests/optimizers/utilities.py
+++ tests/optimizers/utilities.py
@@ -1,24 +1,16 @@
-# This is an example for incorporating the benzene fixture
-from fixtures.benzene import benzene_build
 from scipy.spatial.distance import euclidean
 import numpy as np
 
-import stko
 
-
-def test_benzene(benzene_build):
-
-    # Perform optimisation.
-    optimizer = stko.ETKDG()
-    opt_benzene = optimizer.optimize(benzene_build)
+def compare_molecules(initial_molecule, optimized_molecule):
 
     # Check position matrices.
-    original_pos_mat = benzene_build.get_position_matrix()
-    new_pos_mat = opt_benzene.get_position_matrix()
+    original_pos_mat = initial_molecule.get_position_matrix()
+    new_pos_mat = optimized_molecule.get_position_matrix()
     assert not np.allclose(new_pos_mat, original_pos_mat)
 
     # Check C-C bond lengths.
-    for bond in benzene_build.get_bonds():
+    for bond in initial_molecule.get_bonds():
         atom1 = bond.get_atom1()
         atom2 = bond.get_atom2()
         if (
