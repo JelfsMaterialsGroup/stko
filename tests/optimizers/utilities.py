68db4f3d440db4c06ddbe384e4c807941b7b5854
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-12-03 21:30:37 +0000

    Use RDKIT to find bonds and angles.

--- tests/optimizers/utilities.py
+++ tests/optimizers/utilities.py
@@ -1,13 +1,16 @@
 from scipy.spatial.distance import euclidean
+import rdkit.Chem.AllChem as rdkit
 import numpy as np
 
 
 def compute_angle(atom_a, atom_b, atom_c):
-    # atom_a/b/c are numpy arrays for atomic positions
+    # atom_a/b/c are numpy arrays for atomic positions.
     ba = atom_a - atom_b
     bc = atom_c - atom_b
 
-    cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+    cosine_angle = np.dot(ba, bc) / (
+        np.linalg.norm(ba) * np.linalg.norm(bc)
+    )
     angle = np.arccos(cosine_angle)
     return np.degrees(angle)
 
@@ -18,33 +21,29 @@ def compare_benzenes(initial_molecule, optimized_molecule):
     new_pos_mat = optimized_molecule.get_position_matrix()
     assert not np.allclose(new_pos_mat, original_pos_mat)
 
+    rdkit_mol = optimized_molecule.to_rdkit_mol()
+    rdkit.SanitizeMol(rdkit_mol)
+
     # Check C-C bond lengths.
-    for bond in initial_molecule.get_bonds():
-        atom1 = bond.get_atom1()
-        atom2 = bond.get_atom2()
-        if (
-            atom1.get_atomic_number() == 6
-            and atom2.get_atomic_number() == 6
-        ):
-            bond_length = euclidean(
-                u=new_pos_mat[atom1.get_id()],
-                v=new_pos_mat[atom2.get_id()],
-            )
+    for atom_ids in rdkit_mol.GetSubstructMatches(
+        query=rdkit.MolFromSmarts('[#6]~[#6]'),
+    ):
+        atoms = tuple(optimized_molecule.get_atoms(atom_ids))
+        bond_length = euclidean(
+            u=new_pos_mat[atoms[0].get_id()],
+            v=new_pos_mat[atoms[1].get_id()],
+        )
 
-            assert abs(1.4 - bond_length) < 0.05
-    
+        assert abs(1.4 - bond_length) < 0.05
+
     # Check C-C-C bond angles
-    angles = {x: [] for x in initial_molecule.get_atoms()}
-
-    # Get angle triplets
-    for bond in initial_molecule.get_bonds():
-        atom1 = bond.get_atom1()
-        atom2 = bond.get_atom2()
-        angles[atom1].append(atom2)
-        angles[atom2].append(atom1)
-    
-    for angle in angles.items():
-        atom_a = new_pos_mat[angle[1][0].get_id()]
-        atom_b = new_pos_mat[angle[0].get_id()]
-        atom_c = new_pos_mat[angle[1][1].get_id()]
-        assert abs(compute_angle(atom_a, atom_b, atom_c) - 120) < 5
+    for atom_ids in rdkit_mol.GetSubstructMatches(
+        query=rdkit.MolFromSmarts('[#6]~[#6]~[#6]'),
+    ):
+        atoms = tuple(optimized_molecule.get_atoms(atom_ids))
+        atom1_pos = new_pos_mat[atoms[0].get_id()]
+        atom2_pos = new_pos_mat[atoms[1].get_id()]
+        atom3_pos = new_pos_mat[atoms[2].get_id()]
+        assert abs(
+            compute_angle(atom1_pos, atom2_pos, atom3_pos) - 120
+        ) < 5
