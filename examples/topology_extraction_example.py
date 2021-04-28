e3eb5869a3976ac421181bf909d54889ce0fd61f
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-04-28 11:03:48 +0100

    Write cage example - where disconections are defined by construction for example.

--- examples/topology_extraction_example.py
+++ examples/topology_extraction_example.py
@@ -3,21 +3,30 @@ import stko
 
 
 def main():
-    molecule = stk.BuildingBlock.init_from_file(
-        'output_directory/poly_uff.mol'
+    bb1 = stk.BuildingBlock('NCCN', [stk.PrimaryAminoFactory()])
+    bb2 = stk.BuildingBlock(
+        smiles='O=CC(C=O)C=O',
+        functional_groups=[stk.AldehydeFactory()],
+    )
+    cage1 = stk.ConstructedMolecule(
+        topology_graph=stk.cage.FourPlusSix((bb1, bb2)),
     )
 
+    disconnections = []
+    for bi in cage1.get_bond_infos():
+        if bi.get_building_block() is None:
+            a1id = bi.get_bond().get_atom1().get_id()
+            a2id = bi.get_bond().get_atom2().get_id()
+            disconnections.append(sorted((a1id, a2id)))
+
+    print(disconnections)
     new_topology_graph = stko.TopologyExtractor()
-    print(new_topology_graph)
     new_topology_graph.extract_topology(
-        molecule=molecule,
-        atom_ids_to_disconnect=((6, 18), ),
-        vertex_options={
-            1: stk.polymer.vertices._TerminalVertex,
-        },
+        molecule=cage1,
+        atom_ids_to_disconnect=disconnections,
     )
-    print([i for i in new_topology_graph.get_vertices()])
-    print([i for i in new_topology_graph.get_edges()])
+    print(new_topology_graph.get_vertex_positions())
+    print(new_topology_graph.get_connectivities())
 
 
 if __name__ == "__main__":
