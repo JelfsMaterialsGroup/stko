1fbb32b3e2229a55487688b1b026682f50cc8edd
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-04-30 18:36:21 +0100

    Docs and clean up.

--- stko/molecular/topology_extractor/topology_extractor.py
+++ stko/molecular/topology_extractor/topology_extractor.py
@@ -9,6 +9,7 @@ Class for defining a topology from a molecule and disconnections.
 import logging
 
 from ..networkx import Network
+from .topology_info import TopologyInfo
 
 logger = logging.getLogger(__name__)
 
@@ -22,49 +23,67 @@ class TopologyExtractor:
     def extract_topology(
         self,
         molecule,
-        atom_ids_to_disconnect,
+        broken_bonds_by_id,
+        disconnectors,
     ):
+        """
+        Extract a toplogy defining a molecule with disconnections.
 
-        flat_tuple = []
-        for i in atom_ids_to_disconnect:
-            flat_tuple.append(i[0])
-            flat_tuple.append(i[1])
-        flat_tuple = tuple(set(flat_tuple))
+        Parameters
+        ----------
+        molecule : :class:`stk.Molecule`
+            Molecule to get underlying topology of.
+
+        broken_bonds_by_id : :class:`iterable` of :class:`tuple`
+            Tuples of bonds to break by atom id.
+
+        disconnectors : :class:`set`
+            Atom ids of disconnection points.
+
+        Returns
+        -------
+        :class:`.TopologyInfo`
+            Information of the underlying topology.
+
+        """
 
         connected_graphs = self.get_connected_graphs(
             molecule=molecule,
-            atom_ids_to_disconnect=atom_ids_to_disconnect,
+            atom_ids_to_disconnect=broken_bonds_by_id,
         )
 
-        # Get centroids.
-        self._centroids = {}
-        self._connectivities = {}
+        centroids = {}
+        connectivities = {}
+        edge_pairs = []
         for i, cg in enumerate(connected_graphs):
-            self._centroids[i] = molecule.get_centroid(
-                atom_ids=[i.get_id() for i in list(cg)]
+            centroids[i] = molecule.get_centroid(
+                atom_ids=[i.get_id() for i in cg]
             )
             disconnections = 0
-            for atom in list(cg):
-                aid = atom.get_id()
-                if aid in flat_tuple:
+            for atom in cg:
+                if atom.get_id() in disconnectors:
                     disconnections += 1
-            self._connectivities[i] = disconnections
+            connectivities[i] = disconnections
+
+        for pair in broken_bonds_by_id:
+            a1_g, a2_g = (
+                i for i, cg in enumerate(connected_graphs)
+                if pair[0] in set(atom.get_id() for atom in cg)
+                or pair[1] in set(atom.get_id() for atom in cg)
+            )
+            edge_pairs.append((a1_g, a2_g))
+
+        return TopologyInfo(centroids, connectivities, edge_pairs)
 
     def get_connected_graphs(
         self,
         molecule,
         atom_ids_to_disconnect
     ):
-        graph = Network(molecule)
-        graph.delete_bonds(atom_ids_to_disconnect)
-        connected_graphs = graph.get_connected_graphs()
+        graph = Network.init_from_molecule(molecule)
+        graph = graph.with_deleted_bonds(atom_ids_to_disconnect)
+        connected_graphs = graph.get_connected_components()
         return connected_graphs
-
-    def get_vertex_positions(self):
-        return self._centroids
-
-    def get_connectivities(self):
-        return self._connectivities
 
     def __str__(self):
         return repr(self)
