e9884f2ea9d794eea392910e6edc2652028f4e2a
joshkamm <joshkamm@umich.edu>
2021-05-24 16:19:09 -0400

    work in progress tests for #80

--- tests/calculators/test_torsion_calculators.py
+++ tests/calculators/test_torsion_calculators.py
@@ -1,10 +1,13 @@
 import stko
 import stk
+from examples.torsion_example import get_torsion_info_angles
 
 
 def test_constructed_molecule():
-    bb1 = stk.BuildingBlock('CCCC', [stk.SingleAtom(stk.C(1))])
-    bb2 = stk.BuildingBlock('C', [stk.SingleAtom(stk.C(0))])
+    # bb1 = stk.BuildingBlock('CCCC', [stk.SingleAtom(stk.C(1))])
+    # bb2 = stk.BuildingBlock('C', [stk.SingleAtom(stk.C(0))])
+    bb1 = stk.BuildingBlock('NCCNCCN', [stk.PrimaryAminoFactory()])
+    bb2 = stk.BuildingBlock('O=CCCC=O', [stk.AldehydeFactory()])
 
     polymer = stk.ConstructedMolecule(
         stk.polymer.Linear(
@@ -17,5 +20,10 @@ def test_constructed_molecule():
 
     tors_calculator = stko.ConstructedMoleculeTorsionCalculator()
     tors_results = tors_calculator.get_results(polymer)
+    # print(tors_results.get_molecule())
     # print(tors_results.get_torsion_infos_by_building_block())
-    assert True
+    for t in tors_results.get_torsion_infos():
+        print(f'torsion = {t._torsion}')
+        print(f'building_block_torsion = {t._building_block_torsion}')
+        print(get_torsion_info_angles(polymer, t))
+    assert False
