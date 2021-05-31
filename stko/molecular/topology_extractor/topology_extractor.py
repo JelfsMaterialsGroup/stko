00846de5ec2c16fc27adaf299f1e1fa6c79ed082
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-05-31 12:58:30 +0100

    Add example with smarts.

--- stko/molecular/topology_extractor/topology_extractor.py
+++ stko/molecular/topology_extractor/topology_extractor.py
@@ -20,6 +20,53 @@ class TopologyExtractor:
     """
     Extractor of topology definitions from a molecule.
 
+    Examples
+    --------
+
+    Using a SMARTS string and the
+    :class:`stk.SmartsFunctionalGroupFactory`, you can split a molecule
+    at any point to define a topology.
+
+    .. code-block:: python
+
+        import stk
+        import stko
+
+        smarts = '[#6]~[#7]'
+
+        mol = stk.BuildingBlock(
+            smiles='C1=CC=C(C=C1)CN',
+            functional_groups=(stk.SmartsFunctionalGroupFactory(
+                smarts=smarts,
+                bonders=(),
+                deleters=(),
+            ), )
+        )
+
+        broken_bonds_by_id = []
+        disconnectors = []
+        for fg in mol.get_functional_groups():
+            print(fg)
+            atom_ids = list(fg.get_atom_ids())
+            bond_c = atom_ids[0]
+            bond_n = atom_ids[1]
+            broken_bonds_by_id.append(sorted((bond_c, bond_n)))
+            disconnectors.extend((bond_c, bond_n))
+
+        print(broken_bonds_by_id)
+        print(disconnectors)
+        print('--')
+
+        new_topology_graph = stko.TopologyExtractor()
+        tg_info = new_topology_graph.extract_topology(
+            molecule=mol,
+            broken_bonds_by_id=broken_bonds_by_id,
+            disconnectors=set(disconnectors),
+        )
+        print(tg_info.get_vertex_positions())
+        print(tg_info.get_connectivities())
+        print(tg_info.get_edge_pairs())
+
     """
 
     def extract_topology(
