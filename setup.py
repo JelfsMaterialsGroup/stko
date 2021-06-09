c17b3404652cd7b0381e19f5fe7ff727084e085d
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-06-09 17:04:40 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="stko",
-    version="0.0.33",
+    version="0.0.34",
     author="Steven Bennett, Andrew Tarzia",
     author_email="s.bennett18@imperial.ac.uk",
     description=(
