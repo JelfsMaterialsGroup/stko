3090ed6577682bba638490541b29c0d71c2f3588
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-08-11 20:46:33 +0100

    Write z matrix converter.

--- stko/molecular/__init__.py
+++ stko/molecular/__init__.py
@@ -2,3 +2,4 @@ from .periodic import *  # noqa
 from .topology_extractor import *  # noqa
 from .networkx import *  # noqa
 from .molecule_splitter import *  # noqa
+from .conversion import *  # noqa
