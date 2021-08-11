d5f4d8cdcccb9afca262d796e177f82aabf634ba
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-08-11 19:49:01 +0100

    Remove modifier base class.

--- stko/molecular/__init__.py
+++ stko/molecular/__init__.py
@@ -2,3 +2,4 @@ from .periodic import *  # noqa
 from .topology_extractor import *  # noqa
 from .networkx import *  # noqa
 from .molecule_modifiers import *  # noqa
+from .dummy_atom import *  # noqa
---
+++ stko/molecular/dummy_atom/__init__.py
@@ -0,0 +1,1 @@
+from .dummy_atom import *  # noqa
