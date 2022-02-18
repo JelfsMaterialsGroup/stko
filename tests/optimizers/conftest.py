bd47e9e9cee65ea35119cf5f4d3b2a0f77c872da
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-02-18 14:38:57 +0000

    Improve tests.

--- tests/optimizers/conftest.py
+++ tests/optimizers/conftest.py
@@ -30,16 +30,86 @@ def failing_optimizer():
     return FailingOptimizer()
 
 
-@pytest.fixture
-def unoptimized_mol():
-    return a_molecule()
+class CaseData:
+    """
+    A test case.
+
+    Attributes:
+        molecule:
+            The molecule to be tested.
+
+        unoptimised_energy:
+            The energy of the molecule from stk generation.
+
+        optimised_energy:
+            The energy of the molecule after optimisation.
+
+    """
+
+    position_matrix: np.ndarray
+
+    def __init__(
+        self,
+        molecule,
+        unoptimised_energy,
+        optimised_energy,
+    ):
+
+        self.molecule = molecule
+        self.unoptimised_energy = unoptimised_energy
+        self.optimised_energy = optimised_energy
 
 
-@pytest.fixture
-def optimized_obabel_uff():
-    return 0.5915253852361635
+@pytest.fixture(
+    scope='session',
+    params=[
+        CaseData(
+            molecule=stk.BuildingBlock('NCCN'),
+            unoptimised_energy=98.68403256936926,
+            optimised_energy=43.16966811699349,
+        ),
+        CaseData(
+            molecule=stk.BuildingBlock(
+                'C(#Cc1cccc2ccncc21)c1ccc2[nH]c3ccc(C#Cc4cccc5cnccc54)'
+                'cc3c2c1'
+            ),
+            unoptimised_energy=2032.8111567743895,
+            optimised_energy=805.7039129507456,
+        ),
+        CaseData(
+            molecule=stk.BuildingBlock('CCCCCC'),
+            unoptimised_energy=141.44622279628743,
+            optimised_energy=20.692758531646255,
+        ),
+        CaseData(
+            molecule=stk.BuildingBlock('c1ccccc1'),
+            unoptimised_energy=56.73128282588534,
+            optimised_energy=44.27000519453281,
+        ),
+        CaseData(
+            molecule=stk.ConstructedMolecule(
+                topology_graph=stk.polymer.Linear(
+                    building_blocks=(
+                        stk.BuildingBlock('BrCCBr', [stk.BromoFactory()]),
+                        stk.BuildingBlock(
+                            smiles='BrCNCCBr',
+                            functional_groups=[stk.BromoFactory()],
+                        ),
+                    ),
+                    repeating_unit='AB',
+                    num_repeating_units=2,
+                    optimizer=stk.MCHammer(),
+                ),
+            ),
+            unoptimised_energy=15002.946293854524,
+            optimised_energy=110.38365695234553,
+        ),
+    ],
+)
+def case_molecule(request):
+    """
+    A :class:`.Molecule` instance.
 
+    """
 
-@pytest.fixture
-def unoptimized_obabel_uff():
-    return 30.749185912718534
+    return request.param
--- tests/optimizers/test_obabel_optimizers.py
+++ tests/optimizers/test_obabel_optimizers.py
@@ -5,18 +5,16 @@ from .utilities import (
 )
 
 
-def test_open_babel(
-    unoptimized_mol, unoptimized_obabel_uff, optimized_obabel_uff,
-):
+def test_open_babel(case_molecule):
     energy = (
-        stko.OpenBabelEnergy('uff').get_energy(unoptimized_mol)
+        stko.OpenBabelEnergy('uff').get_energy(case_molecule.molecule)
     )
     optimizer = stko.OpenBabel('uff')
-    opt_res = optimizer.optimize(unoptimized_mol)
-    is_equivalent_molecule(opt_res, unoptimized_mol)
-    inequivalent_position_matrices(opt_res, unoptimized_mol)
+    opt_res = optimizer.optimize(case_molecule.molecule)
+    is_equivalent_molecule(opt_res, case_molecule.molecule)
+    inequivalent_position_matrices(opt_res, case_molecule.molecule)
     opt_energy = (
         stko.OpenBabelEnergy('uff').get_energy(opt_res)
     )
-    assert energy == unoptimized_obabel_uff
-    assert opt_energy == optimized_obabel_uff
+    assert energy == case_molecule.unoptimised_energy
+    assert opt_energy == case_molecule.optimised_energy
