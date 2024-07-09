cae5aca8dac5fd60531d3591b0059e1fa504f3a1
Lukas Turcani <lukasturcani93@gmail.com>
2024-07-09 16:16:49 +0200

    wip

---
+++ examples/openmm_example.py
@@ -0,0 +1,28 @@
+# ruff: noqa: T201
+
+from pathlib import Path
+
+import stk
+import stko
+from openff.toolkit import ForceField
+
+
+def main() -> None:
+    """Run the example."""
+    bb1 = stk.BuildingBlock("C1CCC(C(C1)N)N", [stk.PrimaryAminoFactory()])
+    bb2 = stk.BuildingBlock(
+        "C1=C(C=C(C=C1C=O)C=O)C=O", [stk.AldehydeFactory()]
+    )
+    output = Path("openmm_example_directory")
+    cage = stk.ConstructedMolecule(stk.cage.FourPlusSix([bb1, bb2]))
+    cage.write(output / "unopt_cage.mol")
+    # Load the openff-2.1.0 force field appropriate for
+    # vacuum calculations (without constraints)
+    force_field = ForceField("openff_unconstrained-2.1.0.offxml")
+    optimizer = stko.OpenMMForceField(force_field)
+    cage = optimizer.optimize(cage)
+    cage.write(output / "opt_cage.mol")
+
+
+if __name__ == "__main__":
+    main()
